d837fc99126c576b07de9839a982101a0e8218a9
katarina <katarinarosiak@gmail.com>
2021-05-21 13:07:38 +0200

    ships in the scoreboard added

--- alien_invasion.py
+++ alien_invasion.py
@@ -75,6 +75,8 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -133,12 +135,17 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """
@@ -158,15 +165,15 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left and update scoreboard
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
         else:
             self.stats.game_active = False
             pygame.mouse.set_visible(True)
         # Get rid of any remaining aliens and bullets.
         self.aliens.empty()
         self.bullets.empty()
-        print("hit!!!")
 
         # Create a new fleet and center the ship.
         self._create_fleet()
@@ -216,7 +223,6 @@ class AlienInvasion:
         """Drop the entire fleet and change the fleet's direction."""
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
-            print("fleet drop speed", alien)
         self.settings.fleet_direction *= -1
 
     def _create_alien(self, alien_number, row_number):
